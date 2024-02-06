ff72df3701ade126e7798fb8da554dc3370d0496
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-06 13:14:43 +0300

    added Controller layer

---
+++ SpringJWT/src/main/java/com/bree/springproject/springjwt/controller/AutheticationController.java
@@ -0,0 +1,31 @@
+package com.bree.springproject.springjwt.controller;
+
+import com.bree.springproject.springjwt.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AutheticationController {
+    private final AuthenticationService authService;
+
+    public AuthenticationController(AuthenticationService authService) {
+        this.authService = authService;
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody User request
+    ) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody User request
+    ) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+}
