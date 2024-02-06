d72620e9727606f7dd9e42a7e5331d9de31bf1c0
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-06 13:26:12 +0300

    added authentication service

--- SpringJWT/src/main/java/com/bree/springproject/springjwt/service/AuthenticationService.java
+++ SpringJWT/src/main/java/com/bree/springproject/springjwt/service/AuthenticationService.java
@@ -1,8 +1,38 @@
 package com.bree.springproject.springjwt.service;
 
+import com.bree.springproject.springjwt.model.User;
+import com.bree.springproject.springjwt.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+    }
+
+    public AuthenticationResponse register(User registrationRequest){
+        User user = new User();
+        user.setFirstName(registrationRequest.getFirstName());
+        user.setLastName(registrationRequest.getLastName());
+        user.setUsername(registrationRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
+
+        user.setRole(user.getRole());
+
+        user = userRepository.save(user);
+
+        String token = jwtService.generateToken(user);
+        return new AuthenticationService(token);
+
+
+    }
 
 }
