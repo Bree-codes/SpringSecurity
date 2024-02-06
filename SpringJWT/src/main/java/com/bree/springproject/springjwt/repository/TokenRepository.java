acbce2ea932b42fce34e7be912699dbf8e11655d
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-06 11:21:59 +0300

    added jwt service class

---
+++ SpringJWT/src/main/java/com/bree/springproject/springjwt/repository/TokenRepository.java
@@ -0,0 +1,4 @@
+package com.bree.springproject.springjwt.repository;
+
+public class TokenRepository {
+}
--- SpringJWT/src/main/java/com/bree/springproject/springjwt/service/JwtService.java
+++ SpringJWT/src/main/java/com/bree/springproject/springjwt/service/JwtService.java
@@ -1,8 +1,82 @@
 package com.bree.springproject.springjwt.service;
 
+import com.bree.springproject.springjwt.model.User;
+import com.bree.springproject.springjwt.repository.TokenRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    private final String SECRET_KEY = "2820bff3714b8c6bdbe32d7e21dcfeabea501acd75baeb8a95a3d0e4c9d1d007";
+
+    private final TokenRepository tokenRepository;
+
+    public JwtService(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+
+    public boolean isValid(String token, UserDetails user) {
+        String username = extractUsername(token);
+
+        boolean validToken = tokenRepository
+                .findByToken(token)
+                .map(t -> !t.isLoggedOut())
+                .orElse(false);
+
+        return (username.equals(user.getUsername())) && !isTokenExpired(token) && validToken;
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String generateToken(User user) {
+        String token = Jwts
+                .builder()
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 24*60*60*1000 ))
+                .signWith(getSigninKey())
+                .compact();
+
+        return token;
+    }
+
+    private SecretKey getSigninKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 
 }
