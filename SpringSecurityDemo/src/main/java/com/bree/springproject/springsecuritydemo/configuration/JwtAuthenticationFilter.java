9ca635db88a799a16b718499f303a869664534a4
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-07 15:41:41 +0300

    updates

---
+++ SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,9 @@
+package com.bree.springproject.springsecuritydemo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtAuthenticationFilter {
+
+
+}
