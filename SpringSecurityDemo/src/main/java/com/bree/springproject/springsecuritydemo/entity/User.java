56839a753bad0b35434ebbb73f53e2bbfbb3aa1c
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-07 11:43:58 +0300

    updates

--- SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/entity/User.java
+++ SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/entity/User.java
@@ -3,9 +3,11 @@ package com.bree.springproject.springsecuritydemo.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
@@ -25,31 +27,31 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
