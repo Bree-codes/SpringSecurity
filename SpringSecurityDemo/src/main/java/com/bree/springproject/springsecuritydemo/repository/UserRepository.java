78165cbffce4debc2d77158732329aa9d764247c
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-07 11:19:36 +0300

    updates

--- SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/repository/UserRepository.java
+++ SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.bree.springproject.springsecuritydemo.repository;
 
 import com.bree.springproject.springsecuritydemo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
 
 }
