a3d4d75597b341cece0a0a477d1dc6fa711d2bf2
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-15 10:46:10 +0300

    updates

--- SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/Controller/AdminController.java
+++ SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/Controller/AdminController.java
@@ -1,5 +1,12 @@
 package com.bree.springproject.springsecuritydemo.Controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
 public class AdminController {
 
 }
--- SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/Controller/AuthenticationController.java
+++ SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/Controller/AuthenticationController.java
@@ -1,4 +1,13 @@
 package com.bree.springproject.springsecuritydemo.Controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+
 }
