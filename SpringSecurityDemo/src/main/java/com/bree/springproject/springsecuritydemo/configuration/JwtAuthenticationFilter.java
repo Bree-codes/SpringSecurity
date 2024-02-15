3e180ebde07a4f43ec1bcac471b66bb0debc895d
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-15 08:09:23 +0300

    updates

--- SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/configuration/JwtAuthenticationFilter.java
+++ SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/configuration/JwtAuthenticationFilter.java
@@ -1,9 +1,35 @@
 package com.bree.springproject.springsecuritydemo.configuration;
 
+import com.bree.springproject.springsecuritydemo.service.JwtService;
+import com.bree.springproject.springsecuritydemo.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-@Configuration
-public class JwtAuthenticationFilter {
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+        private final JwtService jwtService;
+        private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+            final String authHeader = request.getHeader("Authorization");
+            final String jwt;
+            final String userEmail;
+
+            if(StringUtils.isEmpty(authHeader) || !StringUtils.s)
+
+    }
 
 
 }
---
+++ SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/service/UserService.java
@@ -0,0 +1,5 @@
+package com.bree.springproject.springsecuritydemo.service;
+
+public class UserService {
+
+}
