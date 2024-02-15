912fce46a0eada061f94ccaccda3e327ce64d3fc
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-15 08:39:20 +0300

    updates

--- SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/service/UserService.java
+++ SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/service/UserService.java
@@ -3,6 +3,6 @@ package com.bree.springproject.springsecuritydemo.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public interface UserService {
 
 }
