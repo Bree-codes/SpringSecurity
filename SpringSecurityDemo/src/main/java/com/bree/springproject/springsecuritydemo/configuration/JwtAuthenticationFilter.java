6eed2aca036b78af53de3871ebc8a014f028d075
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-15 09:28:15 +0300

    updates

--- SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/configuration/JwtAuthenticationFilter.java
+++ SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/configuration/JwtAuthenticationFilter.java
@@ -8,8 +8,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -25,24 +29,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-            final String authHeader = request.getHeader("Authorization");
-            final String jwt;
-            final String userEmail;
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
 
-            if(StringUtils.isEmpty(authHeader) || !org.apache.commons.lang3.StringUtils.startsWith
-                    (authHeader,"Bearer")){
-                filterChain.doFilter(request,response);
-                return;
+        if (StringUtils.isEmpty(authHeader) || !org.apache.commons.lang3.StringUtils.startsWith
+                (authHeader, "Bearer")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUserName(jwt);
+
+        if (StringUtils.isNotEmpty(userEmail) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userService.userDetailsService().loadUserByUsername(userEmail);
+
+            if (jwtService.isTokenValid(jwt, userDetails)){
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,null,userDetails.getAuthorities()
+                        );
+                token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             }
-            jwt = authHeader.substring(7);
-            userEmail = jwtService.extractUserName(jwt);
 
-           if(StringUtils.isNotEmpty(userEmail) && SecurityContextHolder.getContext().getAuthentication()== null){
-               UserDetails userDetails = userService.userDetailsService().loadUserByUsername(userEmail);
-           }
-
-
-
+        }
     }
 
 }
--- SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/service/JwtService.java
+++ SpringSecurityDemo/src/main/java/com/bree/springproject/springsecuritydemo/service/JwtService.java
@@ -9,4 +9,6 @@ public interface JwtService {
     String extractUserName(String token);
 
     String generateToken(UserDetails userDetails);
+
+    boolean isTokenValid(String token,UserDetails userDetails);
 }
