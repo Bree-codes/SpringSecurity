e80e845d7b6ad6ecf4e82a43e216f86c45b67941
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-17 09:29:12 +0300

    updates

--- CustomLogin/src/main/java/springsecutiry/customlogin/configuration/SecurityConfig.java
+++ CustomLogin/src/main/java/springsecutiry/customlogin/configuration/SecurityConfig.java
@@ -1,5 +1,24 @@
 package springsecutiry.customlogin.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.http.HttpRequest;
+
+import static jdk.internal.classfile.Classfile.build;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(form->form.loginPage("/login").permitAll())
+                .authorizeHttpRequests(auth-> auth.anyRequest().authenticated())
+                .build();
+    }
 }
