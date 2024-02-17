f56a475b9e1a3d3ead997ba32d08b846463b4b1f
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-17 09:33:18 +0300

    updates

--- CustomLogin/src/main/java/springsecutiry/customlogin/Controller/LoginController.java
+++ CustomLogin/src/main/java/springsecutiry/customlogin/Controller/LoginController.java
@@ -1,4 +1,13 @@
 package springsecutiry.customlogin.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+
+    @GetMapping("/login")
+    String login(){
+            return "login";
+        }
 }
