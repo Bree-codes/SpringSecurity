443e453f434cd9affbe0947a253b394fc5161077
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-17 09:09:30 +0300

    first commit

---
+++ CustomLogin/src/main/java/springsecutiry/customlogin/CustomLoginApplication.java
@@ -0,0 +1,13 @@
+package springsecutiry.customlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomLoginApplication.class, args);
+	}
+
+}
