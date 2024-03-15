de2d7aa84a7c77dc67f0aad4a48cfddb9a9cbbc8
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-03-15 13:13:18 +0300

    working on securtiy

--- SecurityPractice/src/main/java/com/springboot/springsecurity/service/JwtService.java
+++ SecurityPractice/src/main/java/com/springboot/springsecurity/service/JwtService.java
@@ -1,18 +1,33 @@
 package com.springboot.springsecurity.service;
 
 import com.springboot.springsecurity.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private final String SECRET_KEY = "d905dac76e5b8cf8c7bb2ad5039dddc0701e0a98abcf4806f99221aff7c9362d";
+
+    public <T> T extractClaim(String token, Function<Claims,T> resolver){
+        final Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+    public Claims extractAllClaims(String token){
+        return Jwts
+                .parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 
     public String generateToken(User user){
         String token = Jwts
@@ -25,9 +40,10 @@ public class JwtService {
         return token;
     }
 
-    private Key getSigninKey() {
+    private SecretKey getSigninKey() {
         byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
+
 
 }
