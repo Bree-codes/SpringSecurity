651ffeedd9efbaa3850f5857604bcfa81a4a4a15
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-03-15 13:34:33 +0300

    working on securtiy

--- SecurityPractice/src/main/java/com/springboot/springsecurity/service/JwtService.java
+++ SecurityPractice/src/main/java/com/springboot/springsecurity/service/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -16,10 +17,29 @@ public class JwtService {
 
     private final String SECRET_KEY = "d905dac76e5b8cf8c7bb2ad5039dddc0701e0a98abcf4806f99221aff7c9362d";
 
+    //This function is responsible for extracting a specific claim from the "Claims" object.
     public <T> T extractClaim(String token, Function<Claims,T> resolver){
         final Claims claims = extractAllClaims(token);
         return resolver.apply(claims);
     }
+
+    public String extractUsername(String token){
+        return extractClaim(token,Claims::getSubject);
+    }
+
+    public Boolean isValid(String token, UserDetails userDetails){
+        String username = (extractUsername(token));
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
+    }
+
     public Claims extractAllClaims(String token){
         return Jwts
                 .parser()
