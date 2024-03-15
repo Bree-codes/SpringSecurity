11df73d925b95e017c5af68e82ca1569a5283f9f
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-03-15 11:51:01 +0300

    working on securtiy

---
+++ SecurityPractice/src/main/java/com/springboot/springsecurity/SecurityPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityPracticeApplication.class, args);
+	}
+
+}
---
+++ SecurityPractice/src/test/java/com/springboot/springsecurity/SecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
