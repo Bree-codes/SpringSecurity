ec0c49b43b70170858f19ac5697cd438fcdf8739
Bree-codes <145011976+Bree-codes@users.noreply.github.com>
2024-03-17 10:44:30 +0300

    Update AutheticationController.java

--- SpringJWT/src/main/java/com/bree/springproject/springjwt/controller/AutheticationController.java
+++ SpringJWT/src/main/java/com/bree/springproject/springjwt/controller/AutheticationController.java
@@ -6,7 +6,7 @@ import com.bree.springproject.springjwt.service.AuthenticationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestControlle;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AutheticationController {
